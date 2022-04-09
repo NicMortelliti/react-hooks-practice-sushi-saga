35dd532c0694b0e434e403d5a51b9bf7cce0b528
NicMortelliti <npmortelliti@gmail.com>
2022-04-09 13:15:10 -0700

    Fetch menu data from API and set menu state with return data.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+      .then(r => r.json())
+      .then(menuData => setMenu(menuData));
+  }, []);
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer menu={menu} />
       <Table />
     </div>
   );
