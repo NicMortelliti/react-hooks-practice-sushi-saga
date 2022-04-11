a54ae8fae8f6cb0833de4ce0c5a8fad0747b3133
Nicolas Mortelliti <npmortelliti@gmail.com>
2022-04-11 13:19:09 -0700

    Display only 4 sushi plates at a time.

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -5,18 +5,9 @@ import Sushi from "./Sushi";
 function SushiContainer({ menu, handleEat }) {
   const [inView, setInView] = useState([]);
 
-  // function inViewFn(currentIndex = -1) {
-  //   // Set number of menu items to view at a time
-  //   const currentViewLimit = 4;
-
-  //   for (let index = currentIndex + 1; index < 4; index++) {
-  //     const element = menu[index];
-  //     console.log(element)
-  //   }
-      
-    return (
+  return (
     <div className="belt">
-      {menu.map(menuItem => {
+      {menu.slice(0, 4).map(menuItem => {
         return (
           <Sushi key={menuItem.id} item={menuItem} handleEat={handleEat} />
         );
