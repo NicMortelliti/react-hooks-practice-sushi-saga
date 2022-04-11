f38203c6753ffafaea91010441f182226a33b572
Nicolas Mortelliti <npmortelliti@gmail.com>
2022-04-11 13:20:14 -0700

    Remove unused state variables.

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import MoreButton from "./MoreButton";
 import Sushi from "./Sushi";
 
 function SushiContainer({ menu, handleEat }) {
-  const [inView, setInView] = useState([]);
-
   return (
     <div className="belt">
       {menu.slice(0, 4).map(menuItem => {
